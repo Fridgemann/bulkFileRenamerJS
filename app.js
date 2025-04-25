0e4488b45c48f9b8f872380300b053c80199cc97
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2025-04-25 20:46:47 +0300

    renames files in a test directory, will use commander to add features like extension based renaming

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello')
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
+
+const projectDir = __dirname
+const testDirPath = path.join(projectDir, 'test-dir');
+
+const files = fs.readdirSync(testDirPath);
+
+let count = 0;
+
+files.forEach(file => {
+    if (path.extname(file) === '.txt') {
+        count++;
+        const oldPath = path.join(testDirPath, file);
+        const newPath = path.join(testDirPath, `kemiroglu${count}${path.extname(file)}`);
+        
+        fs.renameSync(oldPath, newPath);
+    }
+})
+
+
+
