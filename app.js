c67d4e177b7b5206a1e1190c96112588b836c3b3
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2025-04-26 17:16:15 +0300

    i have integrated commander, so instead of manually defining directory path in the code itself, you can just pass an argument in the cli with path '<dirnamehere>'

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const { program } = require('commander');
 
-const projectDir = __dirname
-const testDirPath = path.join(projectDir, 'test-dir');
+let testDirPath;
+
+program
+    .version('0.0.1')
+    .description('simple bulk renamer')
+
+
+    program
+    .command('path <dirpath>')
+    .action((dirpath) => {
+        if (dirpath) {
+            testDirPath = dirpath;
+        }
+    });
+
+program.parse(process.argv);
+
 
 const files = fs.readdirSync(testDirPath);
 
 let count = 0;
 
 files.forEach(file => {
-    if (path.extname(file) === '.txt') {
+    if (path.extname(file)) {
         count++;
         const oldPath = path.join(testDirPath, file);
-        const newPath = path.join(testDirPath, `kemiroglu${count}${path.extname(file)}`);
+        const newPath = path.join(testDirPath, `testFileName${count}${path.extname(file)}`);
         
         fs.renameSync(oldPath, newPath);
     }
