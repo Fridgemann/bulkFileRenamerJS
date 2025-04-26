cebc99f618fdb5cabc82477fe756f0707a9a320d
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2025-04-26 21:18:44 +0300

    added a way to provide a name that repeats and a way to target file types through extensions. just be sure to put space between commas like .js, .txt etc

--- app.js
+++ app.js
@@ -2,33 +2,54 @@ const fs = require('fs');
 const path = require('path');
 const { program } = require('commander');
 
-let testDirPath;
+let dirPath;
+let extTypes;
+let name;
 
 program
     .version('0.0.1')
     .description('simple bulk renamer')
 
 
-    program
+program
     .command('path <dirpath>')
-    .action((dirpath) => {
+    .option('-e, --extensions <ext...>', 'filter files by comma seperated extensions (.txt, .md etc)')
+    .option('-n, --name <newFileName>', 'provide new name for files (e.g. newItem1, newItem2)')
+    .action((dirpath, options) => {
         if (dirpath) {
-            testDirPath = dirpath;
-        }
+            dirPath = dirpath;
+            if (options.extensions) {
+                extTypes = options.extensions;
+                console.log(extTypes);
+            }
+            if (options.name) {
+                name = options.name;
+                // console.log(name);
+            };
+        };
     });
 
 program.parse(process.argv);
 
 
-const files = fs.readdirSync(testDirPath);
+const files = fs.readdirSync(dirPath);
 
 let count = 0;
 
 files.forEach(file => {
-    if (path.extname(file)) {
+    if (extTypes.includes(path.extname(file))) {
         count++;
-        const oldPath = path.join(testDirPath, file);
-        const newPath = path.join(testDirPath, `testFileName${count}${path.extname(file)}`);
+
+        const oldPath = path.join(dirPath, file);
+        let newPath;
+
+        if (name) {
+            newPath = path.join(dirPath, `${name}${count}${path.extname(file)}`);
+        }
+        else {
+            newPath = path.join(dirPath, `newFile${count}${path.extname(file)}`);
+        }
+        
         
         fs.renameSync(oldPath, newPath);
     }
