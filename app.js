b6112a258a2de2bec13ca8c2257279e7138df639
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2025-04-26 21:22:31 +0300

    passed the changes on test to main

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const { program } = require('commander');
 
-let testDirPath;
+let dirPath;
+let extTypes;
+let name;
 
 program
     .version('0.0.1')
@@ -15,8 +17,16 @@ program
     .option('-n, --name <newFileName>', 'provide new name for files (e.g. newItem1, newItem2)')
     .action((dirpath, options) => {
         if (dirpath) {
-            testDirPath = dirpath;
-        }
+            dirPath = dirpath;
+            if (options.extensions) {
+                extTypes = options.extensions;
+                console.log(extTypes);
+            }
+            if (options.name) {
+                name = options.name;
+                // console.log(name);
+            };
+        };
     });
 
 program.parse(process.argv);
@@ -29,8 +39,17 @@ let count = 0;
 files.forEach(file => {
     if (extTypes.includes(path.extname(file))) {
         count++;
-        const oldPath = path.join(testDirPath, file);
-        const newPath = path.join(testDirPath, `testFileName${count}${path.extname(file)}`);
+
+        const oldPath = path.join(dirPath, file);
+        let newPath;
+
+        if (name) {
+            newPath = path.join(dirPath, `${name}${count}${path.extname(file)}`);
+        }
+        else {
+            newPath = path.join(dirPath, `newFile${count}${path.extname(file)}`);
+        }
+        
         
         fs.renameSync(oldPath, newPath);
     }
